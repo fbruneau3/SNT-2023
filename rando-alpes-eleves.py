812e5ba9f31d25583fe5467cea43924faebaa6ea
fbruneau <50334315+fbruneau3@users.noreply.github.com>
2023-03-08 16:39:30 +0100

    Add files via upload

---
+++ rando-alpes-eleves.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 20 16:44:04 2019
+
+@author: Propriétaire
+"""
+
+import folium
+c= folium.Map(location=[46.001989 , 6.780061])
+
+
+
+
+c.save('jailacarterando.html')
+
